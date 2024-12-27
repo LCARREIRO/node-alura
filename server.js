0d217f789183304a6542af7de61e76831c401194
lucianocarreirooliveiradasilva <luciano2911@gmail.com>
2024-12-27 00:04:58 -0300

    feat(dbconnection):add mongo db connection and env file

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-import http from 'http';
+import "dotenv/config";
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Cuso de Node.js",
-    "/livros": "Estou na rota livros",
-    "/editora": "Estou na rota editoras"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
-    console.log("servidor escutando");
+app.listen(PORT, () => {
+    console.log("servidor escutando!");
 });
---
+++ src/app.test.js
@@ -0,0 +1,9 @@
+// import buscaLivro from './app.js';
+
+// test('should return book list', () => {
+//     const param = 1;
+//     const expected = {};
+//     let returned = buscaLivro(param);
+
+//     expect(returned).toEqual(expected);
+// })
